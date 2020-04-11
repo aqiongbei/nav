774d022ce7601c34bda1413dd328d932fcaddc4b
aqiongbei <aqiongbei@gmail.com>
2020-04-11 23:39:55 +0800

    add dist

---
+++ dist/precache-manifest.448bb1ba6e57b5b84a62c06f5f884141.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35b5a24203126fc276f",
+    "url": "/css/404.86aa7c43.css"
+  },
+  {
+    "revision": "08cd9c12a7f15b19ca66",
+    "url": "/css/Home.07c2e9e7.css"
+  },
+  {
+    "revision": "252e93f9944dbb3b78e7",
+    "url": "/css/Playground.63a20fb7.css"
+  },
+  {
+    "revision": "16335a7823f031c18ae7",
+    "url": "/css/Settings.0e433876.css"
+  },
+  {
+    "revision": "81b91256eb9c5f9cf034",
+    "url": "/css/app.b54c54d3.css"
+  },
+  {
+    "revision": "ac8ac0b4db082d568bd1",
+    "url": "/css/chunk-vendors.59412c25.css"
+  },
+  {
+    "revision": "143146fa24554ae2c5ac0a3982abb952",
+    "url": "/fonts/ionicons.143146fa.woff2"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "/fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "/fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "/img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "956026cd782714c1c2115ae458d670b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b35b5a24203126fc276f",
+    "url": "/js/404.66eec22a.js"
+  },
+  {
+    "revision": "08cd9c12a7f15b19ca66",
+    "url": "/js/Home.b0de532a.js"
+  },
+  {
+    "revision": "252e93f9944dbb3b78e7",
+    "url": "/js/Playground.6bde0ba7.js"
+  },
+  {
+    "revision": "16335a7823f031c18ae7",
+    "url": "/js/Settings.5b9f2d56.js"
+  },
+  {
+    "revision": "81b91256eb9c5f9cf034",
+    "url": "/js/app.aaa7d79c.js"
+  },
+  {
+    "revision": "ac8ac0b4db082d568bd1",
+    "url": "/js/chunk-vendors.e0c59e1c.js"
+  },
+  {
+    "revision": "eeb6fe12542436f9e90b33d9169a9706",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
